b72fad9b98869361f552f642ea35fc9082f1d3f6
jenniferchristine <jeja2306@student.miun.se>
2024-10-09 16:39:22 +0200

    added 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: "courses", component: CoursesComponent },
-    { path: "", redirectTo: '/courses', pathMatch: 'full' }
+    { path: "", redirectTo: '/courses', pathMatch: 'full' },
+    { path: "404", component: NotFoundComponent }, 
+    { path: "**", redirectTo: "404" }
 ];
